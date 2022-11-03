b3c5f086b020e4c3cbdee0876a31610fddabd8ac
dineshraulo <114489711+dineshraulo@users.noreply.github.com>
2022-11-03 19:15:34 +0530

    this is my src folder

---
+++ src/data.js
@@ -0,0 +1,45 @@
+
+import ora1 from './photos/ora1.jpg';
+import grap from './photos/grap.jpg';
+import water3 from './photos/water3.jpg';
+//import chicken from './photos/chicken.webp';
+
+const data = {
+    productData:[
+        {
+            id:1,
+            img:ora1,
+            title:'orange',
+            desc:'',
+            price:40,
+        },
+            {
+                id:2,
+                img:grap,
+                title:'graps',
+                desc:'',
+                price:50,
+            },
+
+            
+                {
+                    id:3,
+                    img:water3,
+                    title:'watermelon',
+                    desc:'',
+                    price:60,
+                },
+            
+               /* {
+                    id:4,
+                    img:chicken,
+                    title:'chicken',
+                    desc:'',
+                    price:60,
+                },
+              */
+
+    ],
+};
+
+export default data;
